e1cb72269d5ca979150ed7dcea9c747b64c53e0f
Piero Miranda <64490766+PieroMi@users.noreply.github.com>
2022-05-24 16:30:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import os
 import streamlit as st
 import numpy as np
 from PIL import Image
-import _tkinter
+import tkinter
 
 from multipage import Multipage
-from pages import January2021, February2021, March2021, April2021, May2021, June2021, July2021, August2021, September2021, October2021, November2021, December2021
-from pages2 import ProductAnalysis
-
+from pages import January2021, February2021, March2021, April2021, May2021, June2021, July2021, August2021, September2021, October2021, November2021, December2021, January2022, February2022, March2022, April2022
 app = Multipage()
 
 
@@ -23,7 +21,9 @@ app.add_page("September 2021", September2021.app)
 app.add_page("October 2021", October2021.app)
 app.add_page("November 2021", November2021.app)
 app.add_page("December 2021", December2021.app)
-app.add_page("test", ProductAnalysis.app)
-
+app.add_page("January 2022", January2022.app)
+app.add_page("February 2022", February2022.app)
+app.add_page("March 2022", March2022.app)
+app.add_page("April 2022", April2022.app)
 
 app.run()
