14a09958ecabd3d8081237618aae59b1a6541276
Piero Miranda <64490766+PieroMi@users.noreply.github.com>
2022-05-24 16:31:26 -0400

    Update multipage.py

--- multipage.py
+++ multipage.py
@@ -1,13 +1,12 @@
 # To generate mutliple streamlit applications through an object oriented framework
 
+from soupsieve import select
 import streamlit as st
 from PIL import Image
 
 from pages.Report2021 import app
 from pages2.ProductAnalysis import app as app1
-# from pages.January2021 import app as app2
-
-
+from pages3.Report2022 import app as app2
 
 # To manage multiple apps in the program
 class Multipage:
@@ -15,7 +14,6 @@ class Multipage:
     def __init__(self) -> None:
         # Constructor class to generate a list which will store all the applications as an instance variable
         self.pages = []
-        self.pages2 = []
 
     def add_page(self, title, func) -> None:
         # Class method to add pages to the project
@@ -24,12 +22,6 @@ class Multipage:
         #        func: Python function to render the page in streamlit
 
         self.pages.append(
-            {
-                "title" : title,
-                "function" : func
-            }
-        )
-        self.pages2.append(
             {
                 "title" : title,
                 "function" : func
@@ -44,16 +36,18 @@ class Multipage:
         st.image('Posto Letrero.png', use_column_width=None)
 
         pages = {
+          "2022" : "Overview Report of 2022",
           "Main": "Overview Report of 2021",
-          "Monthly": "Monthly Reports of 2021"
+          "Monthly": "Monthly Reports"
         }
-        st.sidebar.markdown("## Data Visualization of 2021")
+
+        st.sidebar.markdown('## **Data Visualization**')
         selected_page = st.sidebar.radio("", pages.values())
 
         if selected_page == pages["Main"]:  
             pages={'title': '', 'function': app }
             pages2={'title': '', 'function': app1 }
-            
+
             pages['function']()
             pages2['function']()
 
@@ -63,5 +57,8 @@ class Multipage:
             self.pages,
             format_func=lambda page: page['title']
         )
-        
             pages['function']()
+
+        elif selected_page == pages['2022']:
+            pages3 = {'title': '', 'function' : app2}
+            pages3['function']()
