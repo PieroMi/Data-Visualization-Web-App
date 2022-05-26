9416fe0f073a8108a8e44dc9117f714c57477e57
Piero Miranda <64490766+PieroMi@users.noreply.github.com>
2022-05-25 21:44:59 -0400

    Update Report2022.py

--- pages3/Report2022.py
+++ pages3/Report2022.py
@@ -11,89 +11,77 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 
+
 def app():
 
     @st.experimental_memo
     def get_data_from_excel():    
-            excel_file = 'POSTO SALES.xlsx'
-            sheet_name = '2021 Report'
-            report2021 = pd.read_excel(excel_file,
+            excel_file = 'POSTO 2022.xlsx'
+            sheet_name = '2022 Report'
+            report2022 = pd.read_excel(excel_file,
                       sheet_name = sheet_name,
                       usecols = 'A:G',
                       nrows = 16)
-            return report2021
-    report2021 = get_data_from_excel()    
+            return report2022
+    report2022 = get_data_from_excel()    
 
-    total_cash = (report2021["Cash"].sum())
-    total_cards = (report2021["Tarjetas"].sum())
-    total_sales = (report2021["Total"].sum())
-    average_sales = total_sales / report2021['Cobrado'].sum()
-    average_per_day = total_sales/ report2021['Days Worked'].sum()
-    expenses = (report2021["Expenses"].sum())
+    total_cash = (report2022["Cash"].sum())
+    total_cards = (report2022["Tarjetas"].sum())
+    total_sales = (report2022["Total"].sum())
+    average_sales = total_sales / report2022['Cobrado'].sum()
+    average_per_day = total_sales/ report2022['Days Worked'].sum()
+    expenses = (report2022["Expenses"].sum())
 
     col1, col2, col3, col4, col5, col6 = st.columns(6)
 
     with col1:
         st.subheader("Cash:")
         st.subheader(f"${total_cash:,.2f}")
+        cashmetric = ((16868.47 / total_cash) * 100)   # Using Up or down trend percentage formula to determine the different in results compared to last years
+        metricresult = f"{100 - cashmetric:,.2f}%"  
+        col1.metric(label = '', value = '', delta = metricresult)  
+        col1.metric(label = '', value = '', delta = f"${total_cash - 16868.47:,.2f}")  
     with col2:
         st.subheader("Cards:")
-        st.subheader(f"${total_cards:,.2f}") 
+        st.subheader(f"${total_cards:,.2f}")
+        cardmetric = ((11385.6 / total_cards) * 100)
+        metricresultcard = f"{100 - cardmetric:,.2f}%"
+        col2.metric(label = '', value = '', delta = metricresultcard)
+        col2.metric(label = '', value = '', delta = f"${total_cards - 11385.6}")    
     with col3:
         st.subheader("Per Day:") 
         st.subheader(f"${average_per_day:,.2f}")   
+        perdaymetric = ((283.2925 / average_per_day) * 100)
+        metricresultperday = f"{100 - perdaymetric:,.2f}%"
+        col3.metric(label = '', value = '', delta = metricresultperday)
+        col3.metric(label = '', value = '', delta = f"${average_per_day - 283.2925:,.2f}")
     with col4:
         st.subheader("Per Bill:")
-        st.subheader(f"${average_sales:,.2f}")    
+        st.subheader(f"${average_sales:,.2f}")
+        averagemetric = ((17.965 / average_sales) * 100)
+        metricresultaverage = f"{100 - averagemetric:,.2f}%"
+        col4.metric(label = '', value = '', delta = metricresultaverage)
+        col4.metric(label = '', value = '', delta = f"${average_sales - 17.965:,.2f}")    
     with col5:
         st.subheader("Expenses:")
-        st.subheader(f"${expenses:,.2f}")  
+        st.subheader(f"${expenses:,.2f}")
+        expensesmetric = ((11682.76 / expenses) * 100)
+        metricresultexpenses = f"{100 - expensesmetric:,.2f}%"
+        col5.metric(label = '', value = '', delta = metricresultexpenses, delta_color = "inverse") 
+        col5.metric(label = '', value = '', delta = f"${expenses - 11682.76:,.2f}", delta_color = "inverse")    
     with col6:
         st.subheader("Total Sales:")
         st.subheader(f"${total_sales:,.2f}")
-    st.markdown("---")  
-
-
-    col6, col7 = st.columns(2)
-
-    @st.experimental_memo
-    def get_data_from_excel():    
-            excel_file = 'POSTO SALES.xlsx'
-            sheet_name = 'HOURS'
-            df = pd.read_excel(excel_file,
-                      sheet_name = sheet_name,
-                      usecols = 'A:B',
-                      nrows = 16)
-            return df
-    df = get_data_from_excel()
-
-    with col6:
-
-        fig = go.Figure(
-            data=[go.Scatter(y = df["Total"], x = df['Hora'])],
-        )
-        fig.update_traces(line_color = "green")
-
-        fig.update_layout(
-        title="Total Vendido Por Hora",
-        xaxis_title="Hora",
-        yaxis_title="Total",
-        font=dict(
-            family="Courier New, monospace",
-            size=15,
-            color="white"
-        ), 
-        plot_bgcolor='rgba(0,0,0,0)' ## plot_bgcolor will remove background color
-    )
-        fig.for_each_xaxis(lambda x: x.update(showgrid=False))  # This will delete the grid for both x and y axis
-        fig.for_each_yaxis(lambda y: y.update(showgrid=False))
-        st.plotly_chart(fig)
-
- 
+        totalmetric = ((28207.07 / total_sales) * 100)
+        metricresulttotal = f"{100 - totalmetric:,.2f}%"
+        col6.metric(label = '', value = '', delta = metricresulttotal) 
+        col6.metric(label = '', value = '', delta = f"${total_sales - 28207.07:,.2f}")
+    st.markdown("*The metrics demonstrate the change in performance compared to 2021's stats*")    
+    st.markdown("---")
 
     @st.experimental_memo
     def get_data_from_excel():    
-        excel_file = 'POSTO SALES.xlsx'
+        excel_file = 'POSTO 2022.xlsx'
         sheet_name = 'EXPENSES'
         df = pd.read_excel(excel_file,
                   sheet_name = sheet_name,
@@ -101,115 +89,73 @@ def app():
                   nrows = 16)
         return df
     df = get_data_from_excel()  
-    
-    with col7:
-        fig0 = go.Bar(
+
+    col7, col8 = st.columns(2)
+    with col8:
+        fig0 = go.Scatter(
         x = df["Fecha"],
-        y = df.Sales,
-        name = 'Sales',
-        marker = dict( color= 'green')
-
+        y = df.Expenses1,
+        mode = 'markers+lines',
+        name = '2021 Expenses',
+        marker = dict( color= 'red')
         )
-
-
         fig2 = go.Scatter(
         x = df["Fecha"],
         y = df.Expenses,
         mode = 'markers+lines',
-        name = 'Expenses', 
-        marker = dict( color= 'red')
+        name = '2022 Expenses', 
+        marker = dict( color= 'green')
         )
-
         data = [fig0, fig2]  ## Created an array to joint both figures in one table
-
-        layout = go.Layout(title = 'Expenses Vs Sales', 
+        layout = go.Layout(title = 'Expenses 2021 Vs 2022', 
                             plot_bgcolor='rgba(0,0,0,0)', # plot_bgcolor will remove the background
                             font=dict(
                             family="Courier New, monospace",
                             size=15,
                             color="white")
                             ) 
-
         figure = go.Figure(data = data, layout = layout)
         figure.for_each_yaxis(lambda y: y.update(showgrid=False)) # This will delete the grid for y axis
-
         st.plotly_chart(figure)
-    st.markdown("---")
 
-    @st.experimental_memo
-    def get_data_from_excel():    
-        excel_file = 'POSTO SALES.xlsx'
-        sheet_name = 'Sub Categories'
-        df3 = pd.read_excel(excel_file,
-                  sheet_name = sheet_name,
-                  usecols = 'A : S',
-                  nrows = 79)
-        return df3
-    df3 = get_data_from_excel()  
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-
-        productlist = df3["Producto"].unique().tolist()
-
-        product = st.multiselect("Select Product to Analyze Sales Per Hour", productlist)
-
-        dfs = {product: df3[df3["Producto"] == product] for product in product}
-
-        fig3 = go.Figure()
-        for product, df3 in dfs.items():
-            fig3 = fig3.add_trace(go.Scatter(x=df3["Hora"], y = df3["Valor"], name = product))
-            fig3.for_each_yaxis(lambda y: y.update(showgrid=False))
-            fig3.for_each_xaxis(lambda x: x.update(showgrid=False))
-            fig3.update_layout(
-            font=dict(
-            family="Courier New, monospace",
-            size=15,
-            color="white"
-        ), plot_bgcolor='rgba(0,0,0,0)')
-
-        st.plotly_chart(fig3)
-
-    with col2:
+    with col7:
         @st.experimental_memo
         def get_data_from_excel():    
-            excel_file = 'POSTO SALES.xlsx'
-            sheet_name = 'WinesAnalysis'
-            wine = pd.read_excel(excel_file,
+            excel_file = 'POSTO 2022.xlsx'
+            sheet_name = 'EXPENSES'
+            df = pd.read_excel(excel_file,
                       sheet_name = sheet_name,
-                      usecols = 'A : C',
+                      usecols = 'E : G',
                       nrows = 16)
-            return wine
-        wine = get_data_from_excel()  
-        
-        sales = go.Bar(
-        x = wine["Fecha"],
-        y = wine.Sales,
-        name = 'Sales',
-        marker = dict( color= 'violet')
+            return df
+        df = get_data_from_excel()  
 
-        )
-        expenses = go.Scatter(
-        x = wine["Fecha"],
-        y = wine.Expenses,
-        mode = 'markers+lines',
-        name = 'Expenses', 
+        fig3 = go.Bar(
+        x = df["Fecha1"],
+        y = df.Sales1,
+        name = '2021 Sales',
         marker = dict( color= 'red')
         )
-
-        data = [sales, expenses]  ## Created an array to joint both figures in one table
-
-        layout = go.Layout(title = 'Wine Expenses Vs Wine Sales', 
+        fig4 = go.Bar(
+        x = df["Fecha1"],
+        y = df.Sales,
+        name = '2022 Sales', 
+        marker = dict( color= 'green')
+        )
+        data = [fig3, fig4]  ## Created an array to joint both figures in one table
+        layout = go.Layout(title = 'Sales 2021 Vs 2022', 
                             plot_bgcolor='rgba(0,0,0,0)', # plot_bgcolor will remove the background
                             font=dict(
                             family="Courier New, monospace",
                             size=15,
                             color="white")
                             ) 
-
         figure = go.Figure(data = data, layout = layout)
         figure.for_each_yaxis(lambda y: y.update(showgrid=False)) # This will delete the grid for y axis
-
         st.plotly_chart(figure)
-    st.markdown("---")
+
+
+
+
+
+
